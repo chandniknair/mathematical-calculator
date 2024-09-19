3dd43635a02770159c849745ab99760396b4f9d7
chandniknair <53247935+chandniknair@users.noreply.github.com>
2024-09-19 16:14:58 +0530

    feat:Temp file

---
+++ temp/calculator.cpp
@@ -0,0 +1,16 @@
+#include "Calculator.h"
+ 
+//Function to add two numbers
+int Calculator::add(int a, int b) {
+    return a + b;
+}
+
+//Function to subtract two numbers
+int Calculator::sub(int a, int b) {
+    return a - b;
+}
+
+//Function to subtract two numbers
+int Calculator::multiply(int a, int b) {
+    return a * b;
+}
---
+++ temp/calculator.h
@@ -0,0 +1,15 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+ 
+class Calculator {
+public:
+    int add(int a, int b);
+};
+
+int sub(int a, int b);
+};
+ 
+
+int multiply(int a, int b);
+};
+#endif // CALCULATOR_H
